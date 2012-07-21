b48c36d2fc25cc6aff186146f7d1a835d1fbb3a6
karl anderson <karl@2600hz.com>
2012-07-21 14:33:32 -0700

    Cleanup tabs

--- src/mod/event_handlers/mod_kazoo/kazoo_bindings.c
+++ src/mod/event_handlers/mod_kazoo/kazoo_bindings.c
@@ -40,10 +40,10 @@ switch_status_t add_event_binding(listener_t *listener, switch_event_types_t *ty
 	/* TODO: contemplate the locks, luckly switch_core_hash* has locked version of each so if we */
 	/*	make it work before mod_kazoo.c handle_event or fs_to_erl_loop step on our toes then */
 	/* 	all we have to do is create the appropriate locks... */
-	
+
 	key = switch_core_sprintf(listener->pool, "<%d.%d.%d>", from->creation, from->num, from->serial);
 	event = switch_core_strdup(listener->pool, switch_event_name(*type));
-        pid = switch_core_alloc(listener->pool, sizeof(erlang_pid));
+	pid = switch_core_alloc(listener->pool, sizeof(erlang_pid));
 	memcpy(pid, from, sizeof(erlang_pid));
 
 	if ((bindings = switch_core_hash_find(listener->event_bindings, event))) {
@@ -57,18 +57,42 @@ switch_status_t add_event_binding(listener_t *listener, switch_event_types_t *ty
 	}
 }
 
+switch_status_t rm_event_pid(listener_t *listener, erlang_pid *from) {
+	switch_hash_index_t *events;
+	switch_hash_t *bindings;
+	erlang_pid *pid;
+
+	remove_key = switch_core_sprintf(listener->pool, "<%d.%d.%d>", from->creation, from->num, from->serial);
+
+	for (events = switch_hash_first(NULL, listener->event_bindings); events; events = switch_hash_next(events)) {
+	    const void *key;
+	    void *value;
+
+        switch_hash_this(events, &key, NULL, &value1);
+        bindings = (switch_hash_t*)value;
+
+		switch_core_hash_delete(bindings, remove_key);
+    }
+
+	return SWITCH_STATUS_SUCCESS;
+}
+
 switch_status_t list_event_bindings(listener_t *listener) {
 	switch_hash_index_t *events, *binding;
 	switch_hash_t *bindings;
-	const void *key1, *key2;
-	void *value1, *value2;
 	erlang_pid *pid;
 
 	for (events = switch_hash_first(NULL, listener->event_bindings); events; events = switch_hash_next(events)) {
+	    const void *key1;
+	    void *value1;
+
 		switch_hash_this(events, &key1, NULL, &value1);
 		bindings = (switch_hash_t*)value1;
 
 		for (binding = switch_hash_first(NULL, bindings); binding; binding = switch_hash_next(binding)) {
+		    const void *key2;
+		    void *value2;
+
 			switch_hash_this(binding, &key2, NULL, &value2);
 			pid = (erlang_pid*)value2;
 			switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_NOTICE, "Found binding %s for %s (<%d.%d.%d>)\n", (char *)key1, (char *)key2, pid->creation, pid->num, pid->serial);
@@ -77,3 +101,14 @@ switch_status_t list_event_bindings(listener_t *listener) {
 	}
 	return SWITCH_STATUS_SUCCESS;
 }
+
+/* For Emacs:
+ * Local Variables:
+ * mode:c
+ * indent-tabs-mode:t
+ * tab-width:4
+ * c-basic-offset:4
+ * End:
+ * For VIM:
+ * vim:set softtabstop=4 shiftwidth=4 tabstop=4:
+ */
