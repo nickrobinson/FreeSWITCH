e462e70a697fabfc7df9dd2815ee10a40d5ba650
root <root@fs002-five9-dfw.2600hz.com>
2013-01-20 07:15:19 +0000

    Created a channel_move module.
    Enhanced mod_sofia to allow skipping of the BYE message intentionally.

--- src/mod/endpoints/mod_sofia/mod_sofia.c
+++ src/mod/endpoints/mod_sofia/mod_sofia.c
@@ -519,6 +519,10 @@ switch_status_t sofia_on_hangup(switch_core_session_t *session)
 		switch_core_session_rwunlock(a_session);
 	}
 
+       if (switch_true(switch_channel_get_variable(channel, "channel_is_moving"))) {
+               goto done;
+       }
+
 	if (sofia_test_pflag(tech_pvt->profile, PFLAG_DESTROY)) {
 		sofia_set_flag(tech_pvt, TFLAG_BYE);
 	} else if (tech_pvt->nh && !sofia_test_flag(tech_pvt, TFLAG_BYE)) {
@@ -670,6 +674,8 @@ switch_status_t sofia_on_hangup(switch_core_session_t *session)
 
 
 	sofia_glue_set_rtp_stats(tech_pvt);
+
+done:
 
 	switch_mutex_unlock(tech_pvt->sofia_mutex);
 
